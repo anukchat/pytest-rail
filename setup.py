660cea15dffa22fae4ac0f16f6d17deba6ca0be9
anukool.chaturvedi <anukool.chaturvedi@avalara.com>
2022-05-02 11:26:11 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ def read_file(fname):
 
 
 setup(
-    name='pytest-testrail',
+    name='pytest-rail',
     description='pytest plugin for creating TestRail runs and adding results',
     long_description=read_file('README.rst'),
-    version='2.9.0',
-    author='Allan Kilpatrick',
-    author_email='allanklp@gmail.com',
-    url='http://github.com/allankp/pytest-testrail/',
+    version='1.0.3,
+    author='Anukool Chaturvedi',
+    author_email='chaturvedianukool@gmail.com',
+    url='http://github.com/anukchat/pytest-rail/',
     packages=[
         'pytest_testrail',
     ],
-    package_dir={'pytest_testrail': 'pytest_testrail'},
+    package_dir={'pytest_rail': 'pytest_rail'},
     install_requires=[
         'pytest>=3.6',
         'requests>=2.20.0',
     ],
     include_package_data=True,
-    entry_points={'pytest11': ['pytest-testrail = pytest_testrail.conftest']},
+    entry_points={'pytest11': ['pytest-rail = pytest_rail.conftest']},
 )
